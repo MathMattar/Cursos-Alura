e7b1edcc3829f951f36617c351c6f4bd3a4364d7
MathMattar <mathmattar@hotmail.com>
2023-02-02 10:14:00 -0300

    02/01

--- 02-JavaScript para Front-end/Curso 06 - JavaScript - Métodos de array/alura_books/app/metodoFilter.js
+++ 02-JavaScript para Front-end/Curso 06 - JavaScript - Métodos de array/alura_books/app/metodoFilter.js
@@ -5,6 +5,28 @@ botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 function filtrarLivros() {
   const elementoBtn = document.getElementById(this.id);
   const categoria = elementoBtn.value;
-  let livrosFiltrados = livros.filter((livro) => livro.categoria == categoria);
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
   exibirOsLivros(livrosFiltrados);
+  if (categoria == "disponivel") {
+    const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+    exibirValorTotalDosLivrosDisponiveisNaTela();
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+  elementoComValorTotalDeLivrosDisponiveis.innehHTML = `    
+  <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+  </div>`;
 }
--- 02-JavaScript para Front-end/Curso 06 - JavaScript - Métodos de array/alura_books/app/metodoForEach.js
+++ 02-JavaScript para Front-end/Curso 06 - JavaScript - Métodos de array/alura_books/app/metodoForEach.js
@@ -1,11 +1,16 @@
 const inserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
 
 function exibirOsLivros(listaDeLivros) {
-    inserirLivros.innerHTML = ""
-    listaDeLivros.forEach((livro) => {
-      inserirLivros.innerHTML += `
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
+  inserirLivros.innerHTML = "";
+  listaDeLivros.forEach((livro) => {
+    let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
+    inserirLivros.innerHTML += `
       <div class="livro">
-          <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}"/>
+          <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}"/>
           <h2 class="livro__titulo">${livro.titulo}</h2>
           <p class="livro__descricao">${livro.autor}</p>
           <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
@@ -13,6 +18,5 @@ function exibirOsLivros(listaDeLivros) {
               <span class="tag">${livro.categoria}</span>
           </div>
       </div>`;
-    });
-  }
-  
+  });
+}
