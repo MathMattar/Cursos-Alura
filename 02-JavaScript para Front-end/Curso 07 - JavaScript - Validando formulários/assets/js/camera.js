647c389147323d560f19395056c61c6e982e66e5
MathMattar <mathmattar@hotmail.com>
2023-02-11 14:30:21 -0300

    Curso JavaScript
    Validando formulários

--- 02-JavaScript para Front-end/Curso 07 - JavaScript - Validando formulários/assets/js/camera.js
+++ 02-JavaScript para Front-end/Curso 07 - JavaScript - Validando formulários/assets/js/camera.js
@@ -2,12 +2,6 @@ const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 
-const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
-const canvas = document.querySelector("[data-video-canvas]");
-const mensagem = document.querySelector("[data-mensagem]");
-
-let imagemURL = "";
-
 botaoIniciarCamera.addEventListener("click", async function () {
   const iniciarVideo = await navigator.mediaDevices.getUserMedia({
     video: true,
@@ -20,6 +14,12 @@ botaoIniciarCamera.addEventListener("click", async function () {
   video.srcObject = iniciarVideo;
 });
 
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+
+let imagemURL = "";
+
 botaoTirarFoto.addEventListener("click", function () {
   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
 
@@ -27,4 +27,17 @@ botaoTirarFoto.addEventListener("click", function () {
 
   campoCamera.style.display = "none";
   mensagem.style.display = "block";
+});
+
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
+
+botaoEnviarFoto.addEventListener("click", () => {
+  const receberDadosExistentes = localStorage.getItem("cadastro");
+  const converteRetorno = JSON.parse(receberDadosExistentes);
+
+  converteRetorno.imagem = imagemURL;
+
+  localStorage.setItem("cadastro", JSON.stringify(converteRetorno));
+
+  window.location.href = "../pages/abrir-conta-form-3.html";
 });
