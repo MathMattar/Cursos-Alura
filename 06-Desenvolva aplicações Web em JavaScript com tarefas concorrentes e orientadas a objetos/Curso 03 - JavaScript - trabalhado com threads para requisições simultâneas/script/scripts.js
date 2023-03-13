b53cc6a22b0f52fbf632d1685e37d8a02b27c291
MathMattar <mathmattar@hotmail.com>
2023-03-13 08:27:14 -0300

    Conclusão: Curso 03 - JavaScript - trabalhado com threads para requisições simultâneas

--- 06-Desenvolva aplicações Web em JavaScript com tarefas concorrentes e orientadas a objetos/Curso 03 - JavaScript - trabalhado com threads para requisições simultâneas/script/scripts.js
+++ 06-Desenvolva aplicações Web em JavaScript com tarefas concorrentes e orientadas a objetos/Curso 03 - JavaScript - trabalhado com threads para requisições simultâneas/script/scripts.js
@@ -1,4 +1,4 @@
-import imprimeCotacao from "./imprimeCotacao.js";
+import selecionaCotacao from "./imprimeCotacao.js";
 
 const graficoDolar = document.getElementById("graficoDolar");
 
@@ -16,21 +16,6 @@ const graficoParaDolar = new Chart(graficoDolar, {
   },
 });
 
-setInterval(() => conectaAPI(), 5000);
-
-async function conectaAPI() {
-  const conecta = await fetch(
-    "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-  );
-
-  const conectaTraduzido = await conecta.json();
-  let tempo = gerarHorario();
-  let valor = conectaTraduzido.USDBRL.ask;
-  adicionarDados(graficoParaDolar, tempo, valor);
-
-  imprimeCotacao("dolar", valor);
-}
-
 function gerarHorario() {
   let data = new Date();
   let horario =
@@ -47,3 +32,63 @@ function adicionarDados(grafico, legenda, dados) {
 
   grafico.update();
 }
+
+let workerDolar = new Worker("./script/workers/workerDolar.js");
+workerDolar.postMessage("usd");
+
+workerDolar.addEventListener("message", (event) => {
+  let tempo = gerarHorario();
+  let valor = event.data.ask;
+  selecionaCotacao("dolar", valor);
+  adicionarDados(graficoParaDolar, tempo, valor);
+});
+
+const graficoIene = document.getElementById("graficoIene");
+const graficoParaIene = new Chart(graficoIene, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Iene",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+let workerIene = new Worker("./script/workers/workerIene.js");
+workerIene.postMessage("iene");
+
+workerIene.addEventListener("message", (event) => {
+  let tempo = gerarHorario();
+  let valor = event.data.ask;
+  adicionarDados(graficoParaIene, tempo, valor);
+  selecionaCotacao("iene", valor);
+});
+
+const graficoEuro = document.getElementById("graficoEuro");
+const graficoParaEuro = new Chart(graficoEuro, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Euro",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+let workerEuro = new Worker("./script/workers/workerEuro.js");
+workerEuro.postMessage("euro");
+
+workerEuro.addEventListener("message", (e) => {
+  let tempo = gerarHorario();
+  let valor = e.data.ask;
+  adicionarDados(graficoParaEuro, tempo, valor);
+  selecionaCotacao("euro", valor);
+});
