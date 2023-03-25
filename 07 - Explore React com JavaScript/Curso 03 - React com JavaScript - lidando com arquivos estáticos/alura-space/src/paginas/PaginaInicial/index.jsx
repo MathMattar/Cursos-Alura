ebc08ebf6dde6245ddd55a89f6a4cfaec4963761
MathMattar <mathmattar@hotmail.com>
2023-03-25 13:12:49 -0300

    Conclusão Curso 03 - React com JavaScript

--- 07 - Explore React com JavaScript/Curso 03 - React com JavaScript - lidando com arquivos estáticos/alura-space/src/paginas/PaginaInicial/index.jsx
+++ 07 - Explore React com JavaScript/Curso 03 - React com JavaScript - lidando com arquivos estáticos/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,10 +1,11 @@
-import Cabecalho from "../../componentes/Cabecalho";
-import Menu from "../../componentes/Menu";
-import Banner from "../../componentes/Banner";
 import style from "./PaginaInicial.module.scss";
-import Rodape from "../../componentes/Rodape";
-import Galeria from "../../componentes/Galeria";
-import Populares from "../../componentes/Populares";
+
+import Cabecalho from "componentes/Cabecalho";
+import Menu from "componentes/Menu";
+import Banner from "componentes/Banner";
+import Rodape from "componentes/Rodape";
+import Galeria from "componentes/Galeria";
+import Populares from "componentes/Populares";
 
 export default function PaginaInicial() {
   return (
@@ -15,10 +16,10 @@ export default function PaginaInicial() {
           <Menu />
           <Banner />
         </div>
-      <div className={style.galeria}>
-        <Galeria />
-        <Populares />
-      </div>
+        <div className={style.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
       </main>
       <Rodape />
     </>
