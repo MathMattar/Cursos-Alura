6454745b92540b72697145688a326b3852bacf57
MathMattar <mathmattar@hotmail.com>
2023-03-25 11:29:14 -0300

    Scripts

---
+++ 07 - Explore React com JavaScript/Curso 03 - React com JavaScript - lidando com arquivos estáticos/alura-space/src/componentes/Galeria/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Galeria.module.scss";
+import Tags from "../Tags";
+import Cards from "./Cards";
+import fotos from "./fotos.json";
+
+export default function Galeria() {
+  return (
+    <section className={styles.galeria}>
+      <h2>Navegue pela galeria</h2>
+      <Tags />
+      <Cards itens={fotos} styles={styles} />
+    </section>
+  );
+}
--- 07 - Explore React com JavaScript/Curso 03 - React com JavaScript - lidando com arquivos estáticos/alura-space/src/paginas/PaginaInicial/index.jsx
+++ 07 - Explore React com JavaScript/Curso 03 - React com JavaScript - lidando com arquivos estáticos/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -3,6 +3,8 @@ import Menu from "../../componentes/Menu";
 import Banner from "../../componentes/Banner";
 import style from "./PaginaInicial.module.scss";
 import Rodape from "../../componentes/Rodape";
+import Galeria from "../../componentes/Galeria";
+import Populares from "../../componentes/Populares";
 
 export default function PaginaInicial() {
   return (
@@ -13,6 +15,10 @@ export default function PaginaInicial() {
           <Menu />
           <Banner />
         </div>
+      <div className={style.galeria}>
+        <Galeria />
+        <Populares />
+      </div>
       </main>
       <Rodape />
     </>
