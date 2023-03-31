bc7fa6d0ea7e24d552a4fd5f7da37ece7401dc20
MathMattar <mathmattar@hotmail.com>
2023-03-31 09:54:11 -0300

    Curso 05 - React: gerenciamento de estados globais com ContextAPI

--- 07-Explore React com JavaScript/Curso 05 - React - gerenciamento de estados globais com ContextAPI/react-context-aula1.2/src/pages/Login/index.js
+++ 07-Explore React com JavaScript/Curso 05 - React - gerenciamento de estados globais com ContextAPI/react-context-aula1.2/src/pages/Login/index.js
@@ -1,23 +1,39 @@
 import { Button } from "@material-ui/core";
 import { Container, Titulo, InputContainer } from "./styles";
 import { Input, InputLabel, InputAdornment } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { UsuarioContext } from "common/context/Usuario";
+import { useContext } from "react";
 
 function Login() {
+  const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
+
   return (
     <Container>
       <Titulo>Insira o seu nome</Titulo>
       <InputContainer>
         <InputLabel>Nome</InputLabel>
-        <Input type="text" />
+        <Input
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          type="text"
+        />
       </InputContainer>
       <InputContainer>
         <InputLabel>Saldo</InputLabel>
         <Input
+          value={saldo}
+          onChange={(e) => setSaldo(e.target.value)}
           type="number"
           startAdornment={<InputAdornment position="start">R$</InputAdornment>}
         />
       </InputContainer>
-      <Button variant="contained" color="primary">
+      <Button
+        onClick={() => history.push("/feira")}
+        variant="contained"
+        color="primary"
+      >
         Avançar
       </Button>
     </Container>
