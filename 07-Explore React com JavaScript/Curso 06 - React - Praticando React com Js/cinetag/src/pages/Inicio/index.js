cb6a6b0137899077e06109c036284dcd6f9d12a3
MathMattar <mathmattar@hotmail.com>
2023-04-18 09:26:01 -0300

    Conclusão - Curso 06 - React: Praticando React com Js

--- 07-Explore React com JavaScript/Curso 06 - React - Praticando React com Js/cinetag/src/pages/Inicio/index.js
+++ 07-Explore React com JavaScript/Curso 06 - React - Praticando React com Js/cinetag/src/pages/Inicio/index.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "components/Card";
 import styles from "./Inicio.module.css";
-import videos from "json/db.json";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/MathMattar/cinetag-api/videos")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
+
   return (
     <>
       <Banner imagem="home" />
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      
+
       <section className={styles.container}>
         {videos.map((video) => {
           return <Card {...video} key={video.id} />;
